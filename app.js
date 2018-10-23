9971224ec24e100386a5707199112bc37f756746
Kubot <kubot.edu@gmail.com>
2018-10-24 01:39:46 +0200

    roll function added

--- app.js
+++ app.js
@@ -8,3 +8,41 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScores, activePlayer, dice;
+
+
+scores = [0 ,0];
+roundScore = 0;
+activePlayer = 1;
+
+//document.querySelector('#current-' + activePlayer).innerHTML = dice;
+/* getter
+var x = document.querySelector('#score-0').textContent;
+*/
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){  // anonymous funciton
+    
+    // roll
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    // display
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    
+    // update
+    
+        
+    
+});
